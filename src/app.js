9982cda59cc9cc343f916f2d759c2b312ca415b4
romeB92 <raghmael@gmail.com>
2023-03-11 21:23:52 +0000

    card gen complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
+function generateCard() {
+  // generator //
+  let suits = ["♦", "♥", "♠", "♣"];
+  let shuffle = Math.random();
+  let totalSuits = suits.length;
+  let shuffleIndex = Math.floor(shuffle * totalSuits);
+  let randomSuit = suits[shuffleIndex];
+  let SuitElements = document.querySelectorAll(".suit");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  SuitElements.forEach(function(suit) {
+    if (randomSuit == "♥" || randomSuit == "♦") {
+      suit.style.color = "red";
+    } else {
+      suit.style.color = "black";
+    }
+    suit.innerHTML = randomSuit;
+  });
+  // Card NUMBER//
+  let cardValue = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  // num generator //
+  let shuffleValue = Math.random();
+  let totalValue = cardValue.length;
+  let shuffleValueIndex = Math.floor(shuffleValue * totalValue);
+  let randomValue = cardValue[shuffleValueIndex];
+  let CardValueElements = document.querySelector(".number");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  CardValueElements.innerHTML = randomValue;
+}
+
+window.onload = generateCard();
